a62dde353df09435528ec60e08237bcd7ce6909f
Fuzzykl <101571662+Fuzzykl@users.noreply.github.com>
2023-10-09 16:48:12 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var ID = document.getElementById("InputID");
 var Submit = document.getElementById("SubmitID");
-var ShowLink = document.getElementById("ShowLink")
+var ShowKey = document.getElementById("ShowKey")
 var regex = /[A-Z]+/;
 
 function RandomKey(){
@@ -12,19 +12,39 @@ function RandomKey(){
     return code;
 }
 
-function RequestKey(key){
-    var Url = `https://thuongpro.name.vn/api/api.php?key=${key}&id_drive=${ID.value}`;
-    fetch(Url);
-    var KeyUrl = `https://tooltranthuong.blogspot.com/p/key_25.html?key=${key}`;
-    ShowLink.innerHTML = `Link Key Của Bạn Đã Được Tạo <a class='Link' href=${KeyUrl}>Ở Đây<a>`;
+function RequestKey(){
+    var key = RandomKey();
+    fetch(`https://thuongpro.name.vn/api/api.php?key=${key}&id_drive=${ID.value}`);
+    for (var i = 2;i>0;i--){
+        var response = fetch(`https://thuongpro.name.vn/key.php?key=${key}`, {
+            "headers": {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "accept-language": "vi,en;q=0.9",
+                "sec-ch-ua": "\"Microsoft Edge\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "object",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "cross-site",
+                "upgrade-insecure-requests": "1"
+            },
+            "referrer": "https://tooltranthuong.blogspot.com/",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
+            "mode": "cors",
+            "credentials": "omit"
+        });
+    }
+    ShowKey.innerHTML = `Key Của Bạn Là : <p class='Key'>${key}<p>`
 }
+
 function Run() {
     if (ID.value === ""){
         alert("Không Được Để ID Trống");
     } else if ((ID.value.trim().indexOf(" ") >= 0)||(regex.test(ID.value))||(ID.value.length < 10)){
         alert("ID Không Đúng");
     } else if (ID.value){ 
-        var Key = RandomKey();
-        RequestKey(Key);
+        RequestKey();
     }
 };
